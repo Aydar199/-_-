734d642d34204a828e5d32df9011c27db9bc8870
Aydar199 <155968152+Aydar199@users.noreply.github.com>
2024-06-05 17:48:17 +0500

    Update setting.py

--- pages/setting.py
+++ pages/setting.py
@@ -1,9 +1,9 @@
-valid_email = 'evgenjamar@mail.ru'
-valid_phone = '+7(926)9390545'
-valid_password = 'poland2012'
-valid_login = 'evgenjamar'
+valid_email = 'gara0216@gmail.com'
+valid_phone = '+7(922)7682868'
+valid_password = 'Aydar1995'
+valid_login = 'gara0216'
 valid_ls = '123456789123'
-valid_username = u"Тестов Ромашка"
+valid_username = u"Гарайшин Айдар"
 ad_slogan = u"Персональный помощник в цифровом мире Ростелекома"
 site = 'https://b2c.passport.rt.ru'
 link_lk = "/account_b2c/page"
